4666158a93dd430adfee1cb09ffd6f86385b3f85
PangPangGod <rhcp1134@gmail.com>
2023-11-08 03:48:37 +0900

    231108 03:48

--- langchain/chroma.py
+++ langchain/chroma.py
@@ -1,14 +1,16 @@
 import os
-import chromadb
-from langchain.vectorstores import Chroma
+from document import BaseDBLoader
 
+from langchain.vectorstores import Chroma
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 
-from langchain.document_loaders import DirectoryLoader, UnstructuredMarkdownLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-
 """ set up vector DB in local path. (./chroma) """
+
+#directory settings
+directory = os.path.dirname(__file__)
+os.chdir(directory)
+
+#embedding config (edit later if domain-adaptation complete.)
 
 def _device_check() : 
     ''' for check cuda availability '''
@@ -16,55 +18,17 @@ def _device_check() :
     device = "cuda" if torch.cuda.is_available() else "cpu"
     return device
 
-#directory settings
-directory = os.path.dirname(__file__)
-os.chdir(directory)
 
-#embedding config (edit later if domain-adaptation complete.)
 embedding = SentenceTransformerEmbeddings(
     model_name="BM-K/KoSimCSE-roberta-multitask", 
     model_kwargs={'device':_device_check()}, 
     encode_kwargs={'normalize_embeddings':True},
     )
 
-#chromadb config
-# persistent_client = chromadb.PersistentClient()
-# collection_name = "vector_db"
-
-# collection = persistent_client.get_or_create_collection(collection_name)
-
-# vectorstore = Chroma(
-#     client=persistent_client,
-#     collection_name = collection_name,
-#     embedding_function = embedding,
-#     persist_directory="./chroma",
-# )
-
-# print("There are", vectorstore._collection.count(), "in the collection.")
-
-
-#textsplitter config
-text_splitter=RecursiveCharacterTextSplitter(
-    separators="\n\n",
-    chunk_size=200,
-    is_separator_regex=False,
-)
-
 #dbsetup
-list_of_path_db = os.listdir("./markdowndb")
-
-result_storage = []
-
-
-### 여기 document.py 만들면 통일 ㄱㄱ
-for db_folder in list_of_path_db:
-    directory_loader = DirectoryLoader(path=os.path.join("./markdowndb", db_folder), loader_cls=UnstructuredMarkdownLoader)
-    result=directory_loader.load_and_split(text_splitter=text_splitter)
-    result_storage.extend(result)
-
+result_storage = BaseDBLoader().load()
 
 ## chroma setting w.langchain (no parentretriever)
 vectorstore = Chroma.from_documents(result_storage, embedding, persist_directory="./chroma")
 vectorstore.persist()
-
 print("There are", vectorstore._collection.count(), "in the collection.")
--- langchain/document.py
+++ langchain/document.py
@@ -1,17 +1,14 @@
 """langchain document 관련해서 db(markdownDB) 불러와서 
-    document로 parse하는 과정 object로 생성. """
+    document로 parse하는 과정 object로 생성."""
 
 import os
+import uuid
 
 from langchain.document_loaders import DirectoryLoader, UnstructuredMarkdownLoader
 from langchain.text_splitter import MarkdownTextSplitter
 
-
-
-
-
 class BaseDBLoader:
-    def __init__(self, path_db: str = "./markdowndb",):
+    def __init__(self, path_db:str="./markdowndb"):
         #textsplitter config
         self.text_splitter = MarkdownTextSplitter(
             chunk_size=200,
@@ -26,6 +23,35 @@ class BaseDBLoader:
         self.path_db = path_db
         self.list_of_path_db = os.listdir(path_db)
 
-    # def load():
+    def load(self)->list:
+        """Generate corpus from langchain document object"""
+        list_of_path_db=self.list_of_path_db
+        text_splitter=self.text_splitter
+        
+        result_storage = []
 
+        for db_folder in list_of_path_db:
+            directory_loader = DirectoryLoader(path=os.path.join(self.path_db, db_folder), loader_cls=UnstructuredMarkdownLoader)
+            result = directory_loader.load_and_split(text_splitter=text_splitter)
+            result_storage.extend(result)     
 
+        return result_storage
+
+class CorpusDBLoader(BaseDBLoader):
+    def _result_to_corpus(self, corpus:dict, result:list)->dict:
+        """Get langchain Document object and convert to dict form with uuid."""
+        for document in result:
+            corpus[str(uuid.uuid4())] = document.page_content
+        return corpus
+    
+    def generate_corpus(self, corpus:dict={})->dict:
+        """Generate corpus from langchain document object"""
+        list_of_path_db = self.list_of_path_db
+        text_splitter = self.text_splitter
+
+        for db_folder in list_of_path_db:
+            directory_loader =  DirectoryLoader(path=os.path.join("./markdowndb", db_folder), loader_cls=UnstructuredMarkdownLoader)
+            result = directory_loader.load_and_split(text_splitter=text_splitter)
+            corpus_piece = self._result_to_corpus({}, result)
+            corpus.update(corpus_piece)
+        return corpus
