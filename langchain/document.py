954404a49404e9f16b701a14e74dc3a0de20b235
kha <jjkha93@gmail.com>
2023-11-09 13:43:38 +0900

    fine code

--- langchain/document.py
+++ langchain/document.py
@@ -10,16 +10,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 
 
-def result_to_regex(result: list[Document]) -> list:
-    regex = '([^가-힣0-9a-zA-Z.,·•%↓()\s\\\])'
-
-    storage = []
-    for document in result:
-        sub_str = re.sub(pattern=regex, repl="$$$$", string=document.page_content)
-        document.page_content = sub_str
-        storage.append(document)
-    return storage
-
 class BaseDBLoader:
     """markdownDB folder에서 불러온 다음에 폴더별로 내부에 있는 내용 Load해서 Split하고 저장함"""
 
@@ -29,6 +19,11 @@ class BaseDBLoader:
             chunk_size=200,
             is_separator_regex=False,
         )
+
+        #필요한 이유 -> llamaindex와 같이 폴더가 나누어져 있는 경우 경로 잡을 때 오류 발생할 가능성 줄이려고.
+        self.directory = os.path.dirname(__file__)
+        os.chdir(self.directory)
+        
         # loaderclass config
         self.loader_cls = loader_cls
         # md 파일 담고 있는 전체 디렉터리 경로
@@ -52,33 +47,8 @@ class BaseDBLoader:
         if is_regex:
             result_storage = result_to_regex(result_storage)
         
-        #필요한 이유 -> llamaindex와 같이 폴더가 나누어져 있는 경우 경로 잡을 때 오류 발생할 가능성 줄이려고.
-        self.directory = os.path.dirname(__file__)
-        os.chdir(self.directory)
-
         return
 
-    def load(self, is_split=True, is_regex=False) -> list[Document]:
-        """Generate corpus from langchain document objects"""
-        for db_folder in os.listdir(self.path_db):
-            db_folder_abs = os.path.join(self.path_db, db_folder)
-            directory_loader = DirectoryLoader(path=db_folder_abs, loader_cls=self.loader_cls)
-
-            if is_split:
-                result = directory_loader.load_and_split(text_splitter=self.text_splitter)
-            else:
-                result = directory_loader.load()
-
-            self.storage.extend(result)
-        #정규식 여부에 따라서 storage 변경
-        if is_regex:
-            self.storage = self._result_to_regex()
-
-    def get_corpus(self) -> dict:
-        return {str(uuid.uuid4()): doc.page_content for doc in self.load(is_split=True, is_regex=False)}
-
-        return self.storage
-    
     def _result_to_regex(self) -> list:
         regex = '([^가-힣0-9a-zA-Z.,·•%↓()\s\\\])'
 
