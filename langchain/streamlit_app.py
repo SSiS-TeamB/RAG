4d71c8d9c0b50ce8630731fd5648d39272b9d9bc
moool01 <tk90941@gmail.com>
2023-11-13 07:13:01 +0900

    231113 07:00 upload

--- langchain/streamlit_app.py
+++ langchain/streamlit_app.py
@@ -3,60 +3,57 @@ import pandas as pd
 import numpy as np
 from tkinter.tix import COLUMN
 # from pyparsing import empty
-import time
+import time, os
+from PIL import Image
+
+
 
 st.set_page_config(layout='wide')
 # add_selectbox = st.sidebar.selectbox("왼쪽 사이드바 Select Box", ("A", "B", "C"))
 
 # 레이아웃
+backgroundColor = "#F0F0F0"
 empty1, con1, empty2 = st.columns([0.3, 1.0, 0.3])
-empty1, con2, con3, empty2 = st.columns([0.3, 1, 0.5, 0.3])
-empty1, con4, empty2 = st.columns([0.3, 1.0, 0.3])
-empty1, con5, con6, empty2 = st.columns([0.3, 0.5, 0.5, 0.3])
-empty1, con7, empty2 = st.columns([0.3, 1.0, 0.3])
-
+empty1, con2, empty2 = st.columns([0.3, 1, 0.3])
+empty1, con3, empty2 = st.columns([0.3, 1.0, 0.3])
+empty1, con4, con5, empty2 = st.columns([0.3, 0.5, 0.5, 0.3])
+empty1, con6, empty2 = st.columns([0.3, 1.0, 0.3])
 
 with con1:
-   st.title("검색 엔진 시스템")
+    st.markdown("<h1 style='text-align: center; color: gray;'>검색 엔진 시스템</h1>", unsafe_allow_html=True)
+    img_ssis = Image.open('ssis_logo.png')
+    img_BL = Image.open('bigleader_logo.png')
+    empty1,col3, col2, col1 = st.columns([1,1, 0.2, 1])
+    col1.image(img_ssis, use_column_width=True)
+    col2.empty()
+    col3.image(img_BL, use_column_width=True)
+    st.markdown("<p style='text-align: right; color: gray;'>무엇이든 물어보세요</p>", unsafe_allow_html=True)
+    # st.image(img_BL)
+    
 #    st.header("Header")
 #    st.subheader("subheader")
-   st.subheader('-'*60)
+#    st.subheader('-'*60)
 #    st.header("아래는 (귀여운 아이)")
 #    st.image("https://static.streamlit.io/examples/cat.jpg", use_column_width=True)
-
 with con2:
-    st.subheader("현재 상황 또는 상태를 적어주시면, 알맞는 복지 서비스를 찾아 드릴게요")
-    query_text = st.text_input('아래에 검색하세요')
+    query_text = st.text_input('검색하셈', label_visibility='collapsed')
+    btn_flag = st.button("click")
     # query_text = st.text_area("이건 여러줄 입력")
-    if query_text:
-        temp = f' "{query_text}"에 대한 정보 검색중...'
-        st.write(temp)
-        # st.button("click button")
-        # st.write("click!!!")
-        progress_text = "당신의 기분에 알맞는 서비스 찾는중. 기대하세요."
+with con3:
+    # st.header("Chart Data")
+    pass
+    # chart_data = pd.DataFrame(np.random.randn(50, 3), columns=["a", "b", "c"])
+    # st.bar_chart(chart_data)
+    # st.write("Write Something")
+with con3:
+    if query_text or btn_flag:
+        progress_text = f'Finding about "{query_text}"...'
         my_bar = st.progress(0, text=progress_text)
 
-        for percent_complete in range(100):
+        for i in range(100):
             time.sleep(0.01)
-            my_bar.progress(percent_complete + 1, text=progress_text)
+            my_bar.progress(i+1, text=progress_text)
         time.sleep(1)
         my_bar.empty()
-
-
-
-
-
-
-##이후에 몇가지 md 를 넣어서 출력 환경 개선하면 된
-    # st.button("Rerun")
-
-# with con3:
-#     st.header("Chart Data")
-#     chart_data = pd.DataFrame(np.random.randn(50, 3), columns=["a", "b", "c"])
-#     st.bar_chart(chart_data)
-#     st.write("Write Something")
-
-
-# vector DB Load
-
-
+        # st.subheader('검색 결과')
+        st.markdown("<h2 style='text-align: center; color: gray;'>검색 결과</h2>", unsafe_allow_html=True)
