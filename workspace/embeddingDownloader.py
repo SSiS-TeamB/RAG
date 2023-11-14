8e85468a7acf17dc59d345836bc74f3eac6ee35b
kha <jjkha93@gmail.com>
2023-11-14 13:47:40 +0900

    불필요한 파일 삭제

--- workspace/embeddingDownloader.py
+++ workspace/embeddingDownloader.py
@@ -9,30 +9,6 @@
 import os
 from sentence_transformers import SentenceTransformer
 
-# class EmbeddingDownLoader:
-#     # model 명 기본으로 지정되어 있음. 다운로드 받으려면 변경해야 함.
-#     def __init__(self, model:str='model_name_here', path=None,) -> None:
-#         self.model = model #model 이름은 huggingface 기준 -> BM-K/KoSimCSE-roberta-multitask
-#         self.path = path
-#         return
-        
-#     def download(self):
-#         # 경로 지정 안해놨으면 현재 스크립트 실행되는 곳에 모델명으로 파일 생성되게 만듦.
-#         if self.path is None:
-#             self.path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.model)
-        
-#         # downloader
-#         downloader = SentenceTransformer(model_name_or_path=self.model)
-#         os.makedirs(self.path)
-#         downloader.save(self.path)
-
-#         print(f'model {self.model} downloaded at path {self.path}.')
-#         return
-
-#test
-# if __name__ == "__main__":
-#     loader = EmbeddingDownLoader(model="BM-K/KoSimCSE-roberta-multitask")
-#     loader.download()
 
 def s_bert_embedding_downloader(model_name, save_path):
     s_transformer = SentenceTransformer(model_name_or_path=model_name)
