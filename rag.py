0988c66b3d59b9693ac17f9ca6e27851ce4c6948
myeongjun1007 <zzikd@naver.com>
2023-11-22 16:21:08 +0900

    update rag.py

--- rag.py
+++ rag.py
@@ -1,22 +1,18 @@
 import os
+import re
 import pickle
 #api key(추가해서 쓰시오)
 from workspace.settings import openai_api_key
-from workspace.analogicalPrompt import generateAnalogicalPrompt, get_normal_prompt
+from workspace.analogicalPrompt import generateAnalogicalPrompt
+from workspace.embeddingSetup import EmbeddingLoader
 
+from langchain.vectorstores.chroma import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.retrievers import BM25Retriever, EnsembleRetriever
-
+from langchain.chains import LLMChain, HypotheticalDocumentEmbedder
+from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema import StrOutputParser
-
-from langchain.retrievers import ParentDocumentRetriever
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.storage import LocalFileStore
-from langchain.storage._lc_store import create_kv_docstore
-
-from chromaVectorStore import ChromaVectorStore
-from workspace.mdLoader import BaseDBLoader 
 
 ###### 시간복잡도 Issue로 HyDE 일단 보류했음. 정확도 측면 제대로 평가하면 쓸 생각.
 ## embedding config - HyDE
@@ -44,87 +40,112 @@ from workspace.mdLoader import BaseDBLoader
 #api key settings
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
+########### object화 해서 llm config, chain, 결과 wrap
+# def temp_rag_pipeline(query:str) -> str:
+
+#     llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
+
+#     embedding = EmbeddingLoader().load()
+#     vectorstore = Chroma(collection_name="wf_schema", persist_directory="workspace/chroma_storage", embedding_function=embedding)
+
+#     #get document from pickle(use as documents in bm25_retriever)
+#     with open('workspace/document.pkl', 'rb') as file :
+#         documents = pickle.load(file)
+
+#     #Vector Search Retriever config
+#     chroma_retriever = vectorstore.as_retriever(search_type='mmr', search_kwargs={"k":5})
+
+#     #BM25 Retriever config
+#     bm25_retriever = BM25Retriever.from_documents(documents=documents)
+#     bm25_retriever.k = 5
+
+#     #ensemble retrievers
+#     ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, chroma_retriever], weights=[0.5, 0.5],)
+
+#     # LCEL
+#     def format_docs(docs):
+#         return "\n\n".join(doc.page_content for doc in docs)
+
+#     rag_chain = (
+#         {"context": ensemble_retriever | format_docs, "question": RunnablePassthrough()}
+#         | generateAnalogicalPrompt()
+#         | llm
+#         | StrOutputParser()
+#     )
+
+#     return rag_chain.invoke(query)
+
 class RAGPipeline:
-    def __init__(self, model, vectorstore, embedding):
-        self.llm = ChatOpenAI(model=model, temperature=0.2)
+    def __init__(self, vectorstore, embedding):
+        self.llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
         self.vectorstore = vectorstore
         self.embedding = embedding
         
-        save_path = "workspace/document.pkl"
-        if not os.path.exists(save_path) :
-            document = BaseDBLoader("workspace/markdownDB").load(is_split=False, is_regex=True)
-            ChromaVectorStore.get_pickle(documents=document, save_path=save_path)
-
         # pickle list 객체 생성 시 로드
         with open('workspace/document.pkl', 'rb') as file:
             self.documents = pickle.load(file)
 
-        child_splitter = RecursiveCharacterTextSplitter(
-            separators=["\n\n", "\n", " ", ""],
-            chunk_size=200,
-            chunk_overlap=10,
-            is_separator_regex=False,
-        )
-
-        #### encode cachefile into byte(to use ParentDocumentRetriever)
-        fs = LocalFileStore("cache")
-        store = create_kv_docstore(fs)
-
-        # ParentDocumentRetriever
-        self.parent_retreiver = ParentDocumentRetriever(
-            vectorstore=vectorstore,
-            docstore=store,
-            child_splitter=child_splitter,
-            search_type="similarity_score_threshold", 
-            search_kwargs={"score_threshold":0.3, "k":5},
-        )
-
-        ## check cachefile exsists 
-        if not list(store.yield_keys()) :
-            dbloader = BaseDBLoader(path_db="workspace/markdownDB/")
-            self.parent_retreiver.add_documents(dbloader.load(is_split=False, is_regex=True))
-
         # Vector Search Retriever
-        # self.chroma_retriever = self.vectorstore.as_retriever(search_type='mmr', search_kwargs={"k":5})
+        self.chroma_retriever = self.vectorstore.as_retriever(search_type='mmr', search_kwargs={"k":5})
 
         # BM25 Retriever
         self.bm25_retriever = BM25Retriever.from_documents(documents=self.documents)
-        self.bm25_retriever.k = 3
+        self.bm25_retriever.k = 5
 
         # Ensemble
-        self.ensemble_retriever = EnsembleRetriever(retrievers=[self.bm25_retriever, self.parent_retreiver], weights=[0.3, 0.7])
+        self.ensemble_retriever = EnsembleRetriever(retrievers=[self.bm25_retriever, self.chroma_retriever], weights=[0.5, 0.5])
 
-        # RAG 체인 구성 With-Analogical Prompting
-        # self.rag_chain = (
-        #     {"context": self.ensemble_retriever | self.format_docs, "question": RunnablePassthrough()}
-        #     | generateAnalogicalPrompt()
-        #     | self.llm
-        #     | StrOutputParser()
-        # )
-        
-        # Non-Analogical Prompting
+        # RAG 체인 구성
         self.rag_chain = (
             {"context": self.ensemble_retriever | self.format_docs, "question": RunnablePassthrough()}
-            | get_normal_prompt()
+            | generateAnalogicalPrompt()
             | self.llm
             | StrOutputParser()
         )
 
     @staticmethod
     def format_docs(docs):
-        return "\n\n\n".join(doc.page_content for doc in docs)
+        
+        ## 어느 제도 부분에서 가져왔는지 나타내는 출처 : medata 활용해서 같이 출력
+        result = []
+        for doc in docs:
+            title_resource = doc.metadata
+            title_resource = str(title_resource).split(":")[1].lstrip()
+            title_resource = re.sub(pattern="}|'",repl="",string=title_resource)
+            title_resource = title_resource.split("\\")[4]+"_"+title_resource.split("\\")[-1]
+            content = doc.page_content 
+            # print(f"** 출처 ** : \n {title_resource}", end="\n\n")
+            # print(f"*** 제도내용 *** : \n {content}", end="\n\n")
+            unit_doc = content + f"\n\n 출처 : {title_resource}"
+            result.append(unit_doc)
+        
+        return "\n\n".join(doc for doc in result)
+        
+
+
+        # # 원래 꺼
+        # return "\n\n".join(doc.page_content for doc in docs)
 
     def invoke(self, query):
         return self.rag_chain.invoke(query)
-    
-    def retrieve(self, query):
-        return self.parent_retreiver.get_relevant_documents(query)
+
+# # 사용 예:
+# pipeline = RAGPipeline()
+# result = pipeline.invoke("질문 내용")
 
 """ ref
 https://python.langchain.com/docs/use_cases/question_answering/vector_db_qa
 https://js.langchain.com/docs/modules/chains/popular/vector_db_qa/
 https://python.langchain.com/docs/use_cases/question_answering/local_retrieval_qa
 """
-# 사용 예:
-# pipeline = RAGPipeline()
-# result = pipeline.invoke("질문 내용")
+
+#setup chain
+# chain = RetrievalQA.from_chain_type(
+#                     llm=llm,
+#                     chain_type="stuff",
+#                     retriever=ensemble_retriever,
+#                     )
+
+##### 예시
+# response = chain.run("당신은 한국의 복지 전문가입니다. 주어진 정보만을 가지고, 다음의 질문에 대답하면 됩니다. 질문 : 농촌 풍수해 피해의 경우 보상받을 수 있는 방법은? 답변 : ...")
+# print(response)
